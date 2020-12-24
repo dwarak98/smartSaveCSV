8c3a99cc9d55faa6a9ce11df8b140d3b7bc6f2ca
dvaradharajan <dvaradharajan@powercosts.com>
2020-12-24 13:48:07 -0500

    Removed other unwanted elemnts from the package

--- R/smartSaveCSV.R
+++ R/smartSaveCSV.R
@@ -1,25 +1,10 @@
-# setwd(dirname(rstudioapi::isAvailable()::getActiveDocumentContext()$path))
-source("R/helperMethods.R")
-library(lubridate)
 
 #' @export smartSaveCSV
 #' @import lubridate
 #' @importFrom utils read.csv write.csv
+library(lubridate)
 
-getWindPenetration <- function(df) {
-  df %>% dplyr::filter(variable %in% c("wind_pen_actual", "wind_pen_STF", "wind_pen_MTF"))
-
-
-}
-
-createBlankCSV <- function(name){
-  file.create(name)
-
-}
-
-
-
-smartSaveCSV <- function(df,csvfile,valuecol) {
+smartSaveCSV <- function(df,path,valuecol) {
   existingdf <- read.csv(path)
   existingdf[[dateColName]] <- ymd_hms(existingdf[[dateColName]])
   existingdf$ModifiedAt <- ymd_hms(existingdf$ModifiedAt)
@@ -29,31 +14,10 @@ smartSaveCSV <- function(df,csvfile,valuecol) {
   newdf <- rbind(existingdf,df) %>%
     arrange(desc(ModifiedAt))
 
- df2 <- newdf[!duplicated(newdf[,distinctcols]),] %>%
+ df2 <- newdf[!duplicated(newdf[,colheader]),] %>%
     write.csv(path,row.names = FALSE)
 
 }
 
-df <- getForecastVsActual() %>%
-  getWindPenetration()
 
-dateColName <- "Interval"
-df[[dateColName]] <- ymd_hms(df[[dateColName]])
-df$ModifiedAt <- Sys.time()
-df$ModifiedAt <- ymd_hms(df$ModifiedAt)
-
-month_year <- format(as.Date(Sys.Date()), "%m_%Y")
-name <- paste("Wpen_Forecast_Vs_Actual_",month_year,".csv",sep="")
-
-day_of_the_month <- format(as.Date(Sys.Date()), "%d")
-path <- paste("Data/",name,sep="")
-if(day_of_the_month ==1){
-
-  createBlankCSV(path)
-  write.csv(df,path,row.names = FALSE)
-}
-# rm(list = c("combine_df_Columns", "format_DateTime", "getForecastVsActual", "getGenMix"))
-
-
-smartSaveCSV(df,path,"value")
 
