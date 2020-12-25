196a52e3e44a3064213eb8dafc4516f8e0acbda7
dvaradharajan <dvaradharajan@powercosts.com>
2020-12-24 19:57:22 -0500

    added style

--- R/smartSaveCSV.R
+++ R/smartSaveCSV.R
@@ -4,20 +4,16 @@
 #' @importFrom utils read.csv write.csv
 library(lubridate)
 
-smartSaveCSV <- function(df,path,valuecol) {
+smartSaveCSV <- function(df, path, valuecol) {
   existingdf <- read.csv(path)
   existingdf[[dateColName]] <- ymd_hms(existingdf[[dateColName]])
   existingdf$ModifiedAt <- ymd_hms(existingdf$ModifiedAt)
-  colheader<- colnames(df)
+  colheader <- colnames(df)
   colheader <- colheader[!colheader %in% valuecol]
 
-  newdf <- rbind(existingdf,df) %>%
+  newdf <- rbind(existingdf, df) %>%
     arrange(desc(ModifiedAt))
 
- df2 <- newdf[!duplicated(newdf[,colheader]),] %>%
-    write.csv(path,row.names = FALSE)
-
+  df2 <- newdf[!duplicated(newdf[, colheader]), ] %>%
+    write.csv(path, row.names = FALSE)
 }
-
-
-
