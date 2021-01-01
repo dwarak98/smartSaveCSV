6885011b52f29b0935d05d0c93057971f8e44b77
dvaradharajan <dvaradharajan@powercosts.com>
2021-01-01 11:55:11 -0500

    Added Readme instructions to the package

--- R/smartSaveCSV.R
+++ R/smartSaveCSV.R
@@ -1,56 +1,32 @@
-#' Production and farm value of maple products in Canada
+#' Append two datasets and contains the latest information
 #'
-#' @source Statistics Canada. Table 001-0008 - Production and farm value of
-#'  maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
+#' This package exports out two functions. the smartAppend() function returns a dataframe which
+#' appends two other dataframes (latestdf, existingdf) and also removes duplicates based on value
+#' column. The smartSaveCSV() function saves the appended dataframe in a csv file.
+#'
 #' @name smartSaveCSV
-#' @param df This is a Data Frame
-#' @param path This is the path to the CSV file
+#' @param latestdf Latest Dataframe whose contents should not be removed when removing duplicates from
+#' the appended dataframe
+#' @param existingdf Old Dataframe whose contents might be removed if duplicates are found after appending
+#' the two dataframes.
+#' @param pathToSave This is the path+Name to save the csv file. Acceptable formats should end with .csv
+#' extension
+#' @param valuecol This parameter is used when removing duplicate rows in the appended dataframe.
+#' Typically, we want to leave the columns mentioned in this parameter and then remove duplicate rows
+#' from rest of the columns. Acceptable formats: c("value","variable","Amount")
+#' @return smartAppend function returns a combined dataframe after removing duplicated rows.
 #'
-#' @format A data frame with columns:
-#' \describe{
-#'  \item{Year}{A value between 1924 and 2015.}
-#'  \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
-#'  \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
-#'  \item{Region}{Postal code abbreviation for territory or province.}
-#' }
+#' @note These two functions checks for the similarities in column datatype. The function
+#' returns an error if any datetime column or date column has incorrect formats. Also, the function
+#' throws a warning when there is differences in classes of columns in two dataframes.
+#'
 #'
 #' @import lubridate
 #' @import pracma
 #' @importFrom utils read.csv write.csv
 library(lubridate)
 library(pracma)
-#' @export
-smartSaveCSV <- function(latestdf, existingdf, pathToSave, valuecol) {
-  colheader1 <- colnames(latestdf)
-  colheader2 <- colnames(existingdf)
-  for (col in colheader1) {
-    if (strcmpi(class(latestdf[[col]])[1], class(existingdf[[col]])[1]) == FALSE && (strcmp(class(latestdf[[col]])[1], "POSIXct") == TRUE || strcmp(class(existingdf[[col]])[1], "POSIXct") == TRUE)) {
-      stop(paste(col, " class does not match between existing df and the latest df"), sep = " ")
-    }
 
-    if (strcmpi(class(latestdf[[col]])[1], class(existingdf[[col]])[1]) == FALSE) {
-      warning(paste(col, " class does not match between existing df and the latest df"), sep = " ")
-    }
-  }
-
-
-  latestdf$ModifiedAtColumn <- Sys.time()
-  latestdf$ModifiedAtColumn <- ymd_hms(latestdf$ModifiedAtColumn)
-  existingdf$ModifiedAtColumn <- Sys.time() - 3600
-  existingdf$ModifiedAtColumn <- ymd_hms(existingdf$ModifiedAtColumn)
-
-
-  appendedDF <- rbind(existingdf, latestdf) %>%
-    arrange(desc(ModifiedAtColumn))
-
-  drops <- c("ModifiedAtColumn")
-  appendedDF <- appendedDF[, !(names(appendedDF) %in% drops)]
-  colheader <- colnames(appendedDF)
-  colheader <- colheader[!colheader %in% valuecol]
-
-  appendedDF[!duplicated(appendedDF[, colheader]), ] %>%
-    write.csv(pathToSave, row.names = FALSE)
-}
 
 #' @export
 smartAppend <- function(latestdf, existingdf, valuecol) {
@@ -82,4 +58,10 @@ smartAppend <- function(latestdf, existingdf, valuecol) {
   colheader <- colheader[!colheader %in% valuecol]
 
   return(appendedDF[!duplicated(appendedDF[, colheader]), ])
+}
+
+#' @export
+smartSaveCSV <- function(latestdf, existingdf, pathToSave, valuecol) {
+  smartAppend(df, existingdf, "value") %>%
+    write.csv(pathToSave, row.names = FALSE)
 }
--- README.md
+++ README.md
@@ -1,11 +1,23 @@
-Untitled
+Smart Save CSV
 ================
+true
 
-## GitHub Documents
+## Objective of this package
 
-This is an R Markdown format used for publishing markdown documents to
-GitHub. When you click the **Knit** button all R code chunks are run and
-a markdown file (.md) suitable for publishing to GitHub is generated.
+This package allows users to combine two dataframes with similar column
+headers and classes. Also, there is an option to remove duplicates using
+value column. valuecol parameter is passed on to the function. The
+function removes duplicate rows by looking at all the column header
+except the ones mentioned in the valuecol parameter. There are many use
+cases for this package, especially related to saving time specific
+information. For example, lets say the user has weather information for
+the last 365 days and also 7 days in the future. Weather information for
+the days in the past does not change but the weather forecast for the
+next 7 days keep changing as we move further into the future. This
+package plays an important role when we have to combined the existing
+weather information, with all the latest information that is going to
+come in the future. Sufficient examples are illustrated below to save
+the user from any confusion.
 
 ## Including Code
 
@@ -13,21 +25,20 @@ You can include R code in the document as follows:
 
 ``` r
 summary(cars)
+#>      speed           dist       
+#>  Min.   : 4.0   Min.   :  2.00  
+#>  1st Qu.:12.0   1st Qu.: 26.00  
+#>  Median :15.0   Median : 36.00  
+#>  Mean   :15.4   Mean   : 42.98  
+#>  3rd Qu.:19.0   3rd Qu.: 56.00  
+#>  Max.   :25.0   Max.   :120.00
 ```
-
-    ##      speed           dist       
-    ##  Min.   : 4.0   Min.   :  2.00  
-    ##  1st Qu.:12.0   1st Qu.: 26.00  
-    ##  Median :15.0   Median : 36.00  
-    ##  Mean   :15.4   Mean   : 42.98  
-    ##  3rd Qu.:19.0   3rd Qu.: 56.00  
-    ##  Max.   :25.0   Max.   :120.00
 
 ## Including Plots
 
 You can also embed plots, for example:
 
-![](README_files/figure-gfm/pressure-1.png)<!-- -->
+![](README-pressure-1.png)<!-- -->
 
 Note that the `echo = FALSE` parameter was added to the code chunk to
 prevent printing of the R code that generated the plot.
